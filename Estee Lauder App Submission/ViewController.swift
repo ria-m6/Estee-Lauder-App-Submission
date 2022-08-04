6972964f11b97f540a9ef60cd23ea850b333ac5b
Ria <riam01206@gmail.com>
2022-08-04 16:56:16 -0700

    Final Project

--- Estee Lauder App Submission/ViewController.swift
+++ Estee Lauder App Submission/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shop: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shopping(_ sender: Any) {
+        let url = URL (string: "https://www.esteelauder.com/product/689/77491/product-catalog/skincare/repair-serum/advanced-night-repair-serum/synchronized-multi-recovery-complex")!
+        UIApplication.shared.open(url)
+    }
+    
 }
 
